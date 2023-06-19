174d2d8dd29304753cf192c01e64095e241851ab
Lorenzinco23 <lorenzinco23@gmail.com>
2023-06-19 17:09:09 +0200

    git includes

---
+++ src/mokaccino.rs
@@ -0,0 +1,13 @@
+use log::{debug, error, info, trace, warn};
+
+
+fn main() {
+    env_logger::init();
+    info!("Trying to print a message to the console");
+    warn!("This is a warning message");
+    error!("This is an error message");
+    debug!("This is a debug message");
+    trace!("This is a trace message");
+
+    //println!("Hello, world!");
+}
--- src/networking/NetworkHandler.rs
+++ src/networking/NetworkHandler.rs
@@ -1,3 +1,9 @@
+use log::debug;
+use log::error;
+use log::info;
+use log::trace;
+use log::warn;
+
 /*
 Mokaccino's network handler class.
 The class takes care of handling all the connections between all the peers.
@@ -16,8 +22,50 @@ struct IpAddr {
 }
 
 struct Peer{
+    active: bool,
     ip_address: IpAddr,
     port: u16,
     username: String,
+    public_key: String,
+}
+
+//network handler class
+pub struct NetworkHandler {
+    peers: Vec<Peer>,
+    server: Peer,
+    port: u16,
+    username: String,
+    public_key: String,
     private_key: String,
 }
+
+impl NetworkHandler {
+    //constructor
+    pub fn new(port: u16, username: String, public_key: String, private_key: String) -> NetworkHandler {
+        !debug!("Creating a new network handler");
+        NetworkHandler {
+            peers: Vec::new(),
+            server: Peer {
+                ip_address: IpAddr {
+                    active: true,
+                    ip_version: IpVersion::ipv4,
+                    ip_address: String::from("mokaccino.ddns.net"),
+                    port: 23232,
+                    username: String::from("server"),
+                }
+            },
+            port: port,
+            username: username,
+            public_key: public_key,
+            private_key: private_key
+        }
+    }
+
+    //getters
+    pub fn get_peers(&self) -> Vec<Peer> {
+        !debug!("Current peers");
+        self.peers
+    }
+
+}
+
